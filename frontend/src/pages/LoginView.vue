161bca71d5d2c77635b094b115d755e49dacfe66
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-07-06 00:36:20 +0800

    add responsiveness in login page

--- frontend/src/pages/LoginView.vue
+++ frontend/src/pages/LoginView.vue
@@ -1,6 +1,7 @@
 <script setup>
     import bg from '../assets/images/login-bg.png'
     import logo from '../assets/images/white-logo.png'
+    import logo2 from '../assets/images/black-logo.png'
     import google from '../assets/images/google.png'
     import email from '../assets/images/email.png'
     import password from '../assets/images/password.png'
@@ -9,59 +10,64 @@
 <template>
     <div id="login">
         <!-- bg -->
-        <div class="w-full h-full overflow-hidden">
-            <!-- bg image -->
-            <img :src="bg" alt="" class="object-center">
-
+        <div  
+            class="hidden md:block bg-[url('login-bg.png')] w-full h-full relative bg-center"
+        >
             <!-- logo -->
-             <img :src="logo" alt="" class="absolute top-15 left-15">
+            <img :src="logo" alt="" class="absolute top-15 left-15">
         </div>
 
         <!-- login form -->
-        <div class="flex-center p-30">
-            <div class="">
-                <h1 class="text-2xl font-semibold text-[var(--color-gray)]">Log in to your Account</h1>
-                <p class="font-light col-center-left text-[var(--color-gray)]">Sign in to access your orders, saved sofas, and personalized recommendations.</p>
+        <div
+            class="flex-center p-5 md:p-30 relative bg-[url('login-bg.png')] md:bg-none bg-cover bg-center"
+        >
+            <!-- form -->
+            <div class="z-50 bg-white/70 backdrop-blur-3xl p-8 md:p-0 rounded-3xl ">
+                <!-- logo -->
+                <h1 class="block md:hidden font-kulim-park text-3xl font-bold mb-5">Modulara</h1>
+
+                <h2 class="md:text-2xl font-semibold text-[var(--color-gray)]">Log in to your Account</h2>
+                <p class="text-sm md:text-lg font-light col-center-left text-[var(--color-gray)]">Sign in to access your orders, saved sofas, and personalized recommendations.</p>
 
                 <!-- sign in with google -->
-                <button class="flex-center gap-3 border w-full border-[#D9D9D9] p-3 mt-3 rounded-lg h-[57px]">
-                    <img :src="google" alt="">
-                    <span class="font-semibold text-[var(--color-gray)] cursor-pointer">Sign in with Google</span>
+                <button class="flex-center gap-3 border w-full border-[#D9D9D9] p-2 md:p-3 mt-3 rounded-lg md:h-[57px] hover:bg-[#D9D9D9] transition delay-100 duration-300 ease-in-out cursor-pointer">
+                    <img :src="google" alt="" class="w-[20px] md:w-auto">
+                    <span class="text-sm md:text-lg font-semibold text-[var(--color-gray)] cursor-pointer">Sign in with Google</span>
                 </button>
 
                 <!-- continue with email -->
                 <div class="flex-center gap-2 mt-4">
-                    <hr class="border border-gray-100 w-full">
-                    <span class="w-full text-sm text-gray-400">or continue with email</span>
-                    <hr class="border border-gray-100 w-full">
+                    <hr class="border border-[#D9D9D9] md:border-gray-100 w-full">
+                    <span class="w-full text-xs md:text-sm text-center text-gray-400">or continue with email</span>
+                    <hr class="border border-[#D9D9D9] md:border-gray-100  w-full">
                 </div>
 
                 <!-- email -->
-                <div class="border border-[#D9D9D9] rounded-lg h-[57px] mt-4 grid grid-cols-[50px_1fr]">
+                <div class="border border-[#D9D9D9] rounded-lg md:h-[57px] mt-4 grid grid-cols-[50px_1fr] p-2">
                     <div class="w-full h-full flex-center">
                        <img :src="email" alt="" class="h-[20px]"> 
                     </div>
-                    <input type="text" class="w-full h-full border-none focus:outline-none" placeholder="Email">
+                    <input type="text" class="text-sm md:text-lg w-full h-full border-none focus:outline-none pr-4" placeholder="Email">
                 </div>
 
                 <!-- password -->
-                <div class="border border-[#D9D9D9] rounded-lg h-[57px] mt-4 grid grid-cols-[50px_1fr]">
+                <div class="border border-[#D9D9D9] rounded-lg md:h-[57px] mt-4 grid grid-cols-[50px_1fr] p-2">
                     <div class="w-full h-full flex-center">
                        <img :src="password" alt="" class="h-[20px]"> 
                     </div>
-                    <input type="password" class="w-full h-full border-none focus:outline-none bg-transparent" placeholder="Password">
+                    <input type="password" class="text-sm md:text-lg w-full h-full border-none focus:outline-none pr-4" placeholder="Password">
                 </div>
 
                 <!-- forgot password -->
                 <div class="mt-3 text-end">
-                    <button class="font-semibold text-[var(--color-gray)] cursor-pointer">Forgot password?</button>
+                    <button class="text-xs md:text-sm font-semibold text-[var(--color-gray)] cursor-pointer">Forgot password?</button>
                 </div>
 
                 <!-- login -->
-                <button class="w-full text-center h-[57px] bg-[var(--color-gray)] mt-7 rounded-lg text-white">Login</button>
+                <button class="w-full text-center md:h-[57px] p-2 bg-[var(--color-gray)] mt-5 md:mt-7 rounded-lg text-white hover:bg-[#202020] transition delay-100 duration-300 ease-in-out cursor-pointer">Login</button>
 
                 <!-- create an account -->
-                <p class="text-center mt-5 text-[#A29F9F] cursor-pointer">Don't have an account? <span class="text-[var(--color-gray)] font-semibold">Create an account</span></p>
+                <p class="text-xs md:text-sm text-center mt-3 md:mt-5 text-[#A29F9F] cursor-pointer">Don't have an account? <span class="text-[var(--color-gray)] font-semibold">Create an account</span></p>
                 
             </div>
             
